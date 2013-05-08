ea2c7b1686457116dd4e0f7dbf53d5d8816cd5b5
matt price <mattprice11@gmail.com>
2013-05-07 23:36:58 -0400

    adding initial mojo files

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 Mojolicious
 ===========
+<pre>
+Mojolicious learning environment
 
-Mojolicious Environment
+Current environment:
+Perl 5.14.3
+Moolicious 3.97
+Fedora 16 64bit
+
+Feel free to use.  
+</pre>
---
+++ testing/Mojolicious/Plugin/MyAuth.pm
@@ -0,0 +1,16 @@
+package Mojolicious::Plugin::MyAuth;
+use Mojo::Base 'Mojolicious::Plugin';
+
+sub register {
+    my ($self, $app) = @_;
+
+    $app->helper(auth => sub {
+        my ($self, $user, $pass) = @_;
+
+        return 1 if
+            $self->param('username') eq $user &&
+            $self->param('password') eq $pass;
+    });
+}
+
+1;
